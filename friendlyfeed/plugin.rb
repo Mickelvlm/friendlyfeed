6fc1a26400be60007462fc509f1e21d8641f75b3
Mickel Munk <3400688+Mickelvlm@users.noreply.github.com>
2020-09-09 14:41:13 +0200

    first steps

---
+++ friendlyfeed/plugin.rb
@@ -0,0 +1,13 @@
+# name: friendlyfeed
+# version: 0.1.0
+
+enabled_site_setting :friendlyfeed_enabled
+
+after_initialize do
+    load File.expand_path('../app/controllers/friendlyfeed_controller.rb', __FILE__)
+  
+    Discourse::Application.routes.append do
+      # Map the path `/friendlyfeed` to `friendlyfeed_Controller`’s `index` method
+      get '/friendlyfeed' => 'friendlyfeed#index'
+    end
+  end
